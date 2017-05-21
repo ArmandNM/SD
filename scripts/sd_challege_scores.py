164b577f5c4dd43318a4c180f8767610ae43bb4a
ArmandNM <armand.nicolicioiu@gmail.com>
2017-05-21 04:15:20 +0300

    added args parser

--- scripts/sd_challege_scores.py
+++ scripts/sd_challege_scores.py
@@ -1,17 +1,39 @@
 import requests
 import json
 import gspread
+import argparse
 from oauth2client.service_account import ServiceAccountCredentials
 
+URL_CHALLENGE = "https://www.hackerrank.com/rest/contests/sd-challenge/leaderboard?offset=0&limit=1000&page%3A1" 
+URL_PARTIAL = "https://www.hackerrank.com/rest/contests/sd-test-practic-partial/leaderboard?offset=0&limit=1000&page%3A1"
+URL_FINAL = "https://www.hackerrank.com/rest/contests/test-practic-final/leaderboard?offset=0&limit=1000&page%3A1"
+
+ARGS = None
+
+
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser(description='Tool for extracting scores from Hackerrank Contests')
+    parser.add_argument("--URL", dest="URL", type=str, default=URL_CHALLENGE)
+    parser.add_argument("--username_column", dest="username_column", type=str, default="D")
+    parser.add_argument("--dest_column", dest="dest_column", type=str, default="E")
+    parser.add_argument("--multiplier", dest="multiplier", type=float, default=1)
+
+    args = parser.parse_args()
+    return args
+
+
 def get_hackerrank_scores():
-    url = "https://www.hackerrank.com/rest/contests/sd-challenge/leaderboard?offset=0&limit=1000&page%3A1"
+    url = ARGS.URL 
     result = requests.get(url)
     leaderboard = json.loads(result.text)['models']
 
     score = {}
 
     for entry in leaderboard:
-        score[entry['hacker']] = int(entry['score'])
+        score[entry['hacker']] = int(entry['score']) * ARGS.multiplier
 
     return score
 
@@ -29,10 +51,13 @@ def update_scores():
     score = get_hackerrank_scores()
     worksheet = get_google_worksheet()
 
-    for i in range(3, 127):
-        username = worksheet.acell("D{}".format(i)).value
+    for i in range(3, 131):
+        username = worksheet.acell("{}{}".format(ARGS.username_column, i)).value
+        print("#{}: {}".format(i, username))
         if username in score:
-            worksheet.update_acell("E{}".format(i), score[username])
+            worksheet.update_acell("{}{}".format(ARGS.dest_column, i), score[username])
 
 
-update_scores()
+if __name__ == "__main__":
+    ARGS = parse_args()
+    update_scores()
